496a9a283e5a7434b7e4b98c67f0bae50c295717
Emre Demirci <eemredemirci@gmail.com>
2020-11-03 00:27:09 +0300

    Started Coding

---
+++ HexagonEmre/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Color color1;
+    public Color color2;
+    public Color color3;
+    public Color color4;
+    public Color color5;
+    public Color color6;
+    public Color color7;
+    public Color color8;
+    public Color color9;
+    public GameObject gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
